9e3dae4a21c07c0a7476106f30faaa202ec45702
Shuhao Wu <shuhao@shuhaowu.com>
2013-10-04 13:10:51 -0400

    Yeah! It's kinda working.
    Now we gotta make it more user friendly and what not.,

--- idp.py
+++ idp.py
@@ -1,11 +1,15 @@
+import base64
+import os
 import json
 import time
 
-from flask import Flask, render_template, abort, jsonify
-from flask.ext.login import LoginManager, UserMixin, login_required, current_user
+from flask import Flask, render_template, abort, jsonify, request
+from flask.ext.login import LoginManager, UserMixin, login_required, current_user, logout_user, login_user
 import bcrypt
+import onetimepass
 import settings
 from browserid.jwt import generate, load_key
+import qrcode
 
 app = Flask(__name__)
 app.config.from_object(settings)
@@ -21,20 +25,36 @@ class User(UserMixin):
       users = json.load(f)
 
     if users.get(email):
-      return User(email, users[email]["hash"], users[email]["salt"])
+      return User(email, users[email]["hash"], users[email]["salt"], users[email]["otp"])
     else:
       return None
 
-  def __init__(self, email, hash, salt):
+  def __init__(self, email, hash, salt, otp_secret):
     self.email = unicode(email)
     self.hash = hash
     self.salt = salt
+    self.otp_secret = otp_secret
 
   def get_id(self):
     return self.email
 
   def verify_password(self, password):
-    return bcrypt.hashpw(password, self.salt) == self.hash
+    return bcrypt.hashpw(str(password), str(self.salt)) == self.hash
+
+  def verify_otp(self, otp):
+    return onetimepass.valid_totp(token=otp, secret=self.otp_secret)
+
+  def verify(self, password, otp):
+    return self.verify_password(password) and self.verify_otp(otp)
+
+  @property
+  def username(self):
+    return self.email.split("@")[0]
+
+
+@login_manager.user_loader
+def user_loader(email):
+  return User.get(email)
 
 
 def make_email(username):
@@ -44,6 +64,11 @@ def make_email(username):
 @app.before_request
 def before_request():
   app.jinja_env.globals["DOMAIN"] = app.config["DOMAIN"]
+  app.jinja_env.globals["DEBUG"] = app.debug
+  if current_user.is_authenticated():
+    app.jinja_env.globals["username"] = current_user.username
+  else:
+    app.jinja_env.globals["username"] = None
 
 
 @app.route("/provisioning")
@@ -60,21 +85,21 @@ def authentication():
 def pwlogin():
   username = request.form["username"]
   password = request.form["password"]
+  try:
+    otp = int(request.form["code"])
+  except:
+    return abort(400)
+
   email = make_email(username)
   user = User.get(email)
   if not user:
     return abort(403)
 
-  if user.verify_password(password):
+  if user.verify(password, otp):
     login_user(user, remember=True)
     return "ok"
   else:
     return abort(403)
-
-
-@app.route("/2fa")
-def twofa():
-  pass
 
 
 @app.route("/signcert", methods=["POST"])
@@ -82,7 +107,7 @@ def twofa():
 def signcert():
   email = request.json.get("email")
   duration = request.json.get("certDuration")
-  public_key = request.json.get("publicKey")
+  public_key = json.loads(request.json.get("publicKey"))
 
   if not email or not duration or not public_key or email != current_user.email:
     return abort(400)
@@ -128,17 +153,31 @@ if __name__ == "__main__":
       username = raw_input("Username: ").strip().lower()
       email = make_email(username)
       password = getpass.getpass()
+      if getpass.getpass("Repeat: ") != password:
+        print "Password do not match!"
+        sys.exit(1)
+
       salt = bcrypt.gensalt()
       hash = bcrypt.hashpw(password, salt)
-      with open("users.json") as f:
-        users = json.load(f)
 
-      users[email] = {"hash": hash, "salt": salt}
+      with open("users.json") as f:
+        try:
+          users = json.load(f)
+        except ValueError:
+          users = {}
+
+      users[email] = {"hash": hash, "salt": salt, "otp": base64.b32encode(os.urandom(16)).strip("=")[:16]}
 
       with open("users.json", "w") as f:
         json.dump(users, f)
 
       print "Added user {}".format(email)
+      print
+      print
+      qr = qrcode.QRCode()
+      qr.add_data("otpauth://totp/{name}?secret={secret}".format(name=email, secret=users[email]["otp"]))
+      qr.print_tty()
+
     elif sys.argv[1] == "del":
       username = raw_input("Username: ").strip().lower()
       email = make_email(username)
--- static/authentication.js
+++ static/authentication.js
@@ -5,14 +5,21 @@ $(function() {
   var cancelBtn = $("#cancel");
   var usernameField = $("#username");
   var passwordField = $("#password");
+  var codeField = $("#code");
+  var loginForm = $("#loginform");
 
-  authBtn.on("click", function(e) {
+  loginForm.on("submit", function(e) {
+    e.preventDefault();
     usernameField.removeClass("invalid");
     passwordField.removeClass("invalid");
+    codeField.removeClass("invalid");
 
     var username = usernameField.val();
     var password = passwordField.val();
+    var code = codeField.val();
 
+    code = $.trim(code);
+    codeField.val(code);
     username = $.trim(username);
     usernameField.val(username);
 
@@ -20,7 +27,11 @@ $(function() {
     if (username.length === 0) {
       usernameField.addClass("invalid");
       valid = false;
-    } else {
+    }
+
+    if (code.length === 0) {
+      codeField.addClass("invalid");
+      valid = false;
     }
 
     if (password.length === 0) {
@@ -33,6 +44,7 @@ $(function() {
 
     usernameField.attr("readonly", "readonly");
     passwordField.attr("readonly", "readonly");
+    codeField.attr("readonly", "readonly");
     authBtn.attr("disabled", "disabled");
     authBtn.text("Checking...");
 
@@ -44,8 +56,10 @@ $(function() {
     var error = function() {
       usernameField.addClass("invalid");
       passwordField.addClass("invalid");
+      codeField.addClass("invalid");
       usernameField.removeAttr("readonly");
       passwordField.removeAttr("readonly");
+      codeField.removeAttr("readonly");
       authBtn.removeAttr("disabled");
       authBtn.text("Invalid credentials");
       setTimeout(function() { authBtn.text("Authenticate"); }, 1000);
@@ -54,11 +68,12 @@ $(function() {
     var req = $.ajax({
       url: "login",
       type: "POST",
-      data: {username: username, password: password},
+      data: {username: username, password: password, code: code},
       success: success,
       error: error
     });
 
+    return false;
   });
 });
 
